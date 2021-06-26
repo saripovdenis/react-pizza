0f3b762c6473e8e7903f3064c4ea0150f589ab2b
Denis Saripov <bblankbboi@gmail.com>
2021-06-26 16:08:21 +0500

    Add a cart logic in redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,14 @@ import Button from './Button';
 import { Link } from 'react-router-dom';
 import logoSvg from '../assets/img/pizza-logo.svg';
 
+import { useSelector } from 'react-redux';
+
 const Header = () => {
+  const { totalPrice, totalCount } = useSelector(({ cart }) => ({
+    totalCount: cart.totalCount,
+    totalPrice: cart.totalPrice,
+  }));
+
   return (
     <div className="header">
       <div className="container">
@@ -19,7 +26,7 @@ const Header = () => {
         <div className="header__cart">
           <Link to="/cart">
             <Button className="button--cart">
-              <span>520 ₽</span>
+              <span>{totalPrice} ₽</span>
               <div className="button__delimiter"></div>
               <svg
                 width="18"
@@ -49,7 +56,7 @@ const Header = () => {
                   strokeLinejoin="round"
                 />
               </svg>
-              <span>3</span>
+              <span>{totalCount}</span>
             </Button>
           </Link>
         </div>
