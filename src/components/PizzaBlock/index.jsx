0ecad0148ef7cb16e27679328f458e836fb735d3
Denis Saripov <bblankbboi@gmail.com>
2021-06-26 16:12:15 +0500

    Button classNames refactor

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
+import Button from '../Button';
+
 function PizzaBlock({ name, imageUrl, price, types, sizes }) {
   const typeNames = ['тонкое', 'традиционное'];
   const sizeValues = [26, 30, 40];
@@ -52,7 +54,7 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
       </div>
       <div className="pizza-block__bottom">
         <div className="pizza-block__price">от {price} ₽</div>
-        <div className="button button--outline button--add">
+        <Button className="button--add" outline>
           <svg
             width="12"
             height="12"
@@ -66,7 +68,7 @@ function PizzaBlock({ name, imageUrl, price, types, sizes }) {
           </svg>
           <span>Добавить</span>
           <i>2</i>
-        </div>
+        </Button>
       </div>
     </div>
   );
