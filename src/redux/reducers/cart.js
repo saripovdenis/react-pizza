9dbb72d42ec9feae781460fd7734b92a8a65a597
Denis Saripov <bblankbboi@gmail.com>
2021-06-29 21:50:56 +0500

    Added a cart info in homepage

--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -7,14 +7,18 @@ const initialState = {
 const cart = (state = initialState, action) => {
   switch (action.type) {
     case 'ADD_PIZZA':
+      const newItems = {
+        ...state.items,
+        [action.payload.id]: !state.items[action.payload.id]
+          ? [action.payload]
+          : [...state.items[action.payload.id], action.payload],
+      };
+      const allPizzas = [].concat.apply([], Object.values(newItems));
       return {
         ...state,
-        items: {
-          ...state.items,
-          [action.payload.id]: !state.items[action.payload.id]
-            ? [action.payload]
-            : [...state.items[action.payload.id], action.payload],
-        },
+        items: newItems,
+        totalCount: allPizzas.length,
+        totalPrice: allPizzas.reduce((sum, obj) => sum + obj.price, 0),
       };
     default:
       return state;
